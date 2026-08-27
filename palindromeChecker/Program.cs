4a432c19bda8a0ad5873f9ca5a4ad6f48e904e72
ahosanul81 <ahosanul81@gmail.com>
2026-08-28 01:17:58 +0600

    wordChecker

---
+++ palindromeChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string words = "kAyak";
+        string lowercaseWords = words.ToLower().Split()[0];
+        string revered = "";
+        for (int i = lowercaseWords.Length - 1; i >= 0; i--)
+        {
+            if (Convert.ToString(lowercaseWords[i]) == " ")
+            {
+                continue;
+            }
+            revered += Convert.ToString(lowercaseWords[i]);
+        }
+        System.Console.WriteLine($"{words} is palimdrome: {revered == lowercaseWords}");
+    }
+}
