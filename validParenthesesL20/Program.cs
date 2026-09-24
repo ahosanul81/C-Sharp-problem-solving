78207d1d2b48a7912a8432db72b934ccdcfe072b
ahosanul81 <ahosanul81@gmail.com>
2026-09-25 01:10:36 +0600

    added solve of 20. valid Parentheses from leetcode

---
+++ validParenthesesL20/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+    public static bool IsValid(string s)
+    {
+        Stack<char> brackets = new Stack<char>();
+        foreach (char b in s)
+        {
+            if (b == '(' || b == '{' || b == '[')
+            {
+                brackets.Push(b);
+            }
+            else
+            {
+                if (brackets.Count() == 0)
+                {
+                    return false;
+                }
+                char top = brackets.Pop();
+
+                if (b == ')' && top != '(')
+                {
+                    return false;
+                }
+                if (b == '}' && top != '{')
+                {
+                    return false;
+                }
+                if (b == ']' && top != '[')
+                {
+                    return false;
+                }
+
+            }
+        }
+        return brackets.Count() == 0;
+    }
+    public static void Main()
+    {
+        bool result = IsValid(s: "([]{}");
+        System.Console.WriteLine(result);
+    }
+}
